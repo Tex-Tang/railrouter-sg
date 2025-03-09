2586abcdf453340c85383e5d2d8bfffa75c7c656
Chun Khai <chunkhai.k@gmail.com>
2025-03-09 12:10:22 +0800

    fix shengsiong geocoding

--- scripts/address_locater.py
+++ scripts/address_locater.py
@@ -1,11 +1,12 @@
 from opencage.geocoder import OpenCageGeocode
+import googlemaps
 import json, os, re, pprint
 
 BASE_DIR = os.path.dirname(__file__)
 json_path = os.path.join(BASE_DIR, "shengsiong-locations.json")
 
-key = 'XXX'
-geocoder = OpenCageGeocode(key)
+key = 'key'
+gmaps = googlemaps.Client(key)
 
 with open(json_path, "r") as f:
     shengsiong_list = json.load(f)
@@ -14,17 +15,17 @@ cleaned_shengsiong_list = []
 error_shengsiong_list = []
 
 for idx, shop in enumerate(shengsiong_list):
-    full_address, time, phone = shop["address"], shop["time"], shop["phone"]
+    full_address, postal_code, is24Hour = shop["address1"], shop["postal_code"], shop["is24Hour"]
     
     cleaned_json = {}
     
-    cleaned_json['is24Hour'] = re.search(r'24', time) is not None
-    cleaned_json['postal_code'] = re.search(r'Singapore (\d+)', full_address).group(1)
-    cleaned_json['address1'] = re.split(r'#', full_address)[0].strip()
+    cleaned_json['is24Hour'] = is24Hour
+    cleaned_json['postal_code'] = postal_code
+    cleaned_json['address1'] = full_address
     
-    results = geocoder.geocode(f'{cleaned_json['address1']}, Singapore')
+    results = gmaps.geocode(f'{cleaned_json['address1']}, Singapore')
     if results:
-        long, lat = results[0]['geometry']['lng'], results[0]['geometry']['lat']
+        long, lat = results[0]['geometry']['location']['lng'], results[0]['geometry']['location']['lat']
         
         if not(1.15 <= lat <= 1.48 and 103.60 <= long <= 104.10):
             cleaned_json['error'] = 'Coordinates Out of Bound'
@@ -40,10 +41,10 @@ for idx, shop in enumerate(shengsiong_list):
 
 BASE_DIR = os.path.dirname(__file__)
 
-output_file = os.path.join(BASE_DIR, "cleaned_shengsiong_locations.json")
+output_file = os.path.join(BASE_DIR, "new-shengsiong-locations.json")
 with open(output_file, "w", encoding="utf-8") as outfile:
     json.dump(cleaned_shengsiong_list, outfile, indent=4)
 
-error_file = os.path.join(BASE_DIR, "error_shengsiong_locations.json")
+error_file = os.path.join(BASE_DIR, "error-shengsiong-locations.json")
 with open(error_file, "w", encoding="utf-8") as outfile:
     json.dump(error_shengsiong_list, outfile, indent=4)
